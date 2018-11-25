86d0fad440e2cd59e6d6a3ef25fa61e50fbb5cea
Pete Davison <pd93.uk@outlook.com>
2018-11-25 03:50:21 +0000

    Split findTags into two functions to allow finding tags by pattern - A text document is parsed in instead of reading from the current active editor

--- src/utils.ts
+++ src/utils.ts
@@ -1,48 +1,53 @@
 'use strict';
 
-// Libraries
 import * as vscode from 'vscode';
 import * as structures from './structures';
 
-const window = vscode.window;
-const editor = window.activeTextEditor;
+export function findTagsByPattern(pattern: structures.Pattern, document: vscode.TextDocument): structures.Tag[] {
 
-// FindTags will scan the active text editor for matches to the configured tag patterns
-export function findTags(patterns: structures.Pattern[]): structures.Tag[] {
-
-    console.log("Finding tags...");
-
-    if (!editor || !editor.document) {
-        return [];
-    }
+    console.log(`Finding tags for pattern: '${pattern.name}'...`);
 
     // Init
     let tags: structures.Tag[] = [];
-    let re: RegExp;
-
-    // Get the document text
-    let text = editor.document.getText();
-
-    // Loop through each tag
-    for (let i = 0; i < patterns.length; i++) {
-
-        let pattern: structures.Pattern = patterns[i];
-        let match: RegExpExecArray | null;
-        
-        // Create a new regex object
-        if (pattern.caseSensitive) {
-            re = new RegExp(pattern.pattern, 'g');
-        } else {
-            re = new RegExp(pattern.pattern, 'gi');
-        }
-
-        // Loop through all the matches and add them to an array
-        while (match = re.exec(text)) {
-            tags.push({name: pattern.name, start: match.index, end: match.index+match[0].length});
-        }
+    let match: RegExpExecArray | null;
+    let re;
+    
+    // Create a new regex object
+    if (pattern.caseSensitive) {
+        re = new RegExp(pattern.pattern, 'g');
+    } else {
+        re = new RegExp(pattern.pattern, 'gi');
     }
 
-    console.log("Found " + tags.length + " tag" + (tags.length === 1 ? "" : "s"));
+    // Loop through all the matches and add them to an array
+    while (match = re.exec(document.getText())) {
+        tags.push({
+            name: pattern.name,
+            text: match[0],
+            filepath: document.uri.fsPath,
+            start: document.positionAt(match.index),
+            end: document.positionAt(match.index + match[0].length)
+        });
+    }
+
+    console.log(tags);
+
+    return tags;
+}
+
+// FindTags will scan the active text editor for matches to the configured tag patterns
+export function findTags(patterns: structures.Pattern[], document: vscode.TextDocument): structures.Tag[] {
+
+    console.log("Finding tags...");
+
+    // Init
+    let tags: structures.Tag[] = [];
+
+    // Loop through each tag
+    for (let pattern of patterns) {
+        tags.push(...findTagsByPattern(pattern, document));
+    }
+
     console.log(tags);
 
     return tags;
