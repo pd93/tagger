56a8de3d78c89c89e2214514caf62127dbcb81fd
Pete Davison <pd93.uk@outlook.com>
2018-12-01 03:36:57 +0000

    Removed old settings

--- src/settings.ts
+++ src/settings.ts
@@ -14,13 +14,11 @@ export class Settings {
         this.updateOn = config.get("updateOn") || "change";
         this.include = config.get("include") || "**/*";
         this.exclude = config.get("exclude") || "";
-        this.maxResults = config.get("maxResults") || 100;
         this.patterns = config.get("patterns") || [];
     }
 
     public updateOn: string;
     public include: string;
     public exclude: string;
-    public maxResults: number;
     public patterns: Pattern[];
 }
